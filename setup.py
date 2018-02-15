46f6b7ddc42d112e0f2611bd45e9f4a51b552115
Seb Bacanu <sebastian@arista.com>
2018-02-15 20:11:26 +0000

    Fix issues with pypi distribution

--- setup.py
+++ setup.py
@@ -1,22 +1,28 @@
-from distutils.core import setup
+from setuptools import setup
+
+with open('./requirements.pip') as dependencies_file:
+    dependencies = dependencies_file.read().split('\n')
+    dependencies = filter(lambda dep: len(dep) > 0, dependencies)
 
 setup(
     name='telemetry_email_alerter',
-    packages=['telemetry_email_alerter'],
-    version='1.0',
+    version='1.0.1',
     description='Script for subscribing to Arista Telemetry Events and sending email alerts.',
     author='Seb Bacanu',
     author_email='sebastian@arista.com',
     url='https://github.com/aristanetworks/telemetry-email-alerter',
-    download_url='https://github.com/aristanetworks/telemetry-email-alerter/archive/v1.0.tar.gz',
-    keywords=[
-        'alerts',
-        'arista',
-        'cvp',
-        'email',
-        'notifications',
-        'smtp',
-        'telemetry',
+    download_url='https://github.com/aristanetworks/telemetry-email-alerter/archive/v1.0.0.tar.gz',
+    keywords='alerts arista cvp email notifications smtp telemetry',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
     ],
-    classifiers=[],
+    packages=['telemetry_email_alerter'],
+    install_requires=dependencies,
+    entry_points={
+        'console_scripts': [
+            'telemetry_email_alerter = telemetry_email_alerter:main',
+        ]
+    }
 )
